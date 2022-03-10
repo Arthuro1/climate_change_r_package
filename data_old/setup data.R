7007cc86f4aa029a652dd760f19d90631c9f08e9
Paul Meteng <paul.meteng-kamdem@stud.uni-due.de>
2022-03-10 23:52:40 +0100

    update dataset

--- data_old/setup data.R
+++ data_old/setup data.R
@@ -1,27 +1,21 @@
 rm(list = ls())
 
-heatData_us=read.csv("./data_old/heat-wave-index-usa.csv", sep=",")
-heatData_us = heatData_us[heatData_us[,"Year"] >= 1901,c(4)]
+heat_waves_index_us=read.csv("./data_old/heat-wave-index-usa.csv", sep=",")
+heat_waves_index_us = heat_waves_index_us[heat_waves_index_us[,"Year"] >= 1901,c(4)]
 
-droughtData=read.csv("data_old/drought-severity-index-us.csv", sep=",")
-droughtData_us = droughtData[droughtData[,1] == "United States",]
-droughtData_us = droughtData_us[droughtData_us[,"Year"] >= 1901,c(3,5)]
+drougth_severity_index=read.csv("data_old/drought-severity-index-us.csv", sep=",")
+drougth_severity_index_us = drougth_severity_index[drougth_severity_index[,1] == "United States",]
+drougth_severity_index_us = drougth_severity_index_us[drougth_severity_index_us[,"Year"] >= 1901,c(3,5)]
 
-#temperatureData_us=read.csv("data_old/temperature-us.csv", sep=",")
-temperatureData_us = read.csv("data_old/temperature-us-new.csv", sep=",")
-temperatureData_us = temperatureData_us[temperatureData_us[,"Date"] >= 190112,c(2)]
+temperature_us = read.csv("data_old/temperature-us-new.csv", sep=",")
+temperature_us = temperature_us[temperature_us[,"Date"] >= 190112,c(2)]
 
-#co2Data=read.csv("data_old/co-emissions-per-capita.csv", sep=",")
-co2Data = read.csv("data_old/annual-co2-emissions.csv", sep=",")
-co2Data_us = co2Data[co2Data[,1] == "United States",]
-co2Data_us = co2Data_us[co2Data_us[,"Year"] >= 1901,c(4)]
-co2Data_us = co2Data_us/max(co2Data_us)
+co2_emission = read.csv("data_old/annual-co2-emissions.csv", sep=",")
+co2_emission_us = co2_emission[co2_emission[,1] == "United States",]
+co2_emission_us = co2_emission_us[co2_emission_us[,"Year"] >= 1901,c(4)]
 
-#co2Data_de_new = co2Data_new[co2Data_new[,1] == "Germany",]
-#co2Data_de_new = co2Data_de_new[co2Data_de_new[,"Year"] >= 1901,c(4)]
-#co2Data_de_new = co2Data_de_new/max(co2Data_de_new)
+heavy_precipitation_area_us = read.csv("data_old/heavy-precip_2.csv")
+heavy_precipitation_area_us = heavy_precipitation_area_us[heavy_precipitation_area_us[, "Year"] >= 1901, c(2)]
 
-climateset = cbind(droughtData_us, co2Data_us, temperatureData_us, heatData_us)
-
-#sorted_co2_de_data <- sort(co2Data_de_new, index.return = TRUE)
-#plot(sorted_co2_de_data$x, co2Data_us_new[sorted_co2_de_data$ix], type = "l")
+climateset = as.data.frame(cbind(drougth_severity_index_us, co2_emission_us, temperature_us, heat_waves_index_us, heavy_precipitation_area_us))
+save(climateset, file = "data/climateset.RData")
